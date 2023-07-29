dab55e3070319c1febd1a7b6298079e3556a1352
TogrulEminov <togrul40@mail.ru>
2023-07-29 23:13:35 +0400

    .

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,14 +1,14 @@
-import ContentWrapper from "../../components/contentWrapper/ContentWrapper"
-import "./style.scss"
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
+import './style.scss';
 const NotFound = () => {
   return (
     <div className="pageNotFound">
-            <ContentWrapper>
-                <span className="bigText">404</span>
-                <span className="smallText">Page not found!</span>
-            </ContentWrapper>
-        </div>
-  )
-}
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
